0ff6ffbba8b89adb2b03a28820923092682f78a6
jcldyc <jacobcharlesli@gmail.com>
2017-09-15 00:39:21 -0500

    Update simplechain8.c

--- simplechain8.c
+++ simplechain8.c
@@ -4,9 +4,11 @@
 
 int main (int argc, char *argv[]){
 	pid_t childpid = 0;
-	int i, n, c;
+	int i, n, c, j=0;
+	nchars=argv[2];
+	char mybuf[strlen(nchars)+1]="";
 
-	if (argc != 2){
+	if (argc != 3){
 		fprintf (stderr, "Usage: %s processes\n", argv[0]);
 		return 1;
 	}
@@ -14,21 +16,26 @@ int main (int argc, char *argv[]){
 	while((c = getopt (argc, argv, "h")) != -1)
 		switch(c){
 			case 'h':
-				fprintf(stderr,"Use:  %s <int>\n",argv[0], argv[0]);
+				fprintf(stderr,"Use:  %s <int> <nchars>\n",argv[0], argv[0]);
 				break;
 			default:
-				perror(" simplechain: Error:" );
+				perror(" simplechain8: Error:" );
 				break;
 	}
-
-
-
 	n = atoi(argv[1]);
 	for (i = 1; i < n; i++)
 		if(childpid = fork())
 			break;
+	
+	while(argv[2][j] != '\0')
+       		mybuf[j]=nchars[j];   
+		j++;
+    	}
+	mybuf[j+1]='\0';
 
-	fprintf(stderr, "i:%d process ID: %ld parent ID: %ld child ID: %ld\n",
-		i, (long)getpid(), (long)getppid(), (long)childpid);
+	fprintf(stderr, "process ID: %ld: %s", (long)getpid(), nchars);
+	
+	
+	
 	return 0;
 }
